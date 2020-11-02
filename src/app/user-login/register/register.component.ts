c7022d21395e2b1fec047f28b9a821825d3c64bd
riisj <riisjesper@hotmail.com>
2020-11-02 01:33:01 +0100

    Added registraion html page

--- src/app/user-login/register/register.component.ts
+++ src/app/user-login/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {FormControl, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -7,9 +8,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
+  registerForm = new FormGroup({
+    firstName: new FormControl(''),
+    lastName: new FormControl(''),
+    email: new FormControl('', Validators.email),
+    address: new FormControl(''),
+    password: new FormControl(''),
+    password2: new FormControl('')
+  });
+
+  hide = true;
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+  onSubmit(): void {
+    console.log(this.registerForm.get('firstName').value);
+    console.log(this.registerForm.get('lastName').value);
+    console.log(this.registerForm.get('email').value);
+    console.log(this.registerForm.get('address').value);
+    console.log(this.registerForm.get('password').value);
+    console.log(this.registerForm.get('password').value);
   }
 
 }
